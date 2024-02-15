b3965abc08c53403183ab09c76d78c3ded10bc44
jayjaria <jaijaria032001@gmail.com>
2024-02-15 19:33:45 +0530

    commit

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
@@ -34,8 +34,36 @@ def late_entry():
 
     return jsonify({'message':'Late Entry recorded successfully'}), 201
 
-
+# 1. Gets the count of student in all time
 @app.route('/late_count/<roll_number>', methods=['GET'])
 def late_count(roll_number):  #We'll fetch roll number from the URL
     count = LateEntry.query.filter(LateEntry.roll_number==roll_number).count()
+
     return jsonify({'roll_number':roll_number, 'late_count':count})
+
+
+# 2. Checks if a Student was late on last night
+@app.route('/check_late_last_night/<roll_number>', methods=['GET'])
+def check_late_last_night(roll_number):
+    today = datetime.utcnow().date()
+    yesterday = today - timedelta(days=1)
+
+    late_last_night = LateEntry.query.filter(LateEntry.roll_number==roll_number, LateEntry.entry_time__date==yesterday).count()
+
+    was_late = late_last_night>0
+
+    return jsonify({'roll_number':roll_number,'was_late':was_late})
+
+# 3. Finds all late enrties in last night
+@app.route('/late_entries_last_night', methods=['GET'])
+def late_entries_last_night():
+    today = datetime.utcnow().date()
+    yesterday = today - timedelta(days=1)
+
+    late_entries = LateEntry.query.filter(LateEntry.entry_time__date==yesterday).all()
+
+    late_students = [{'roll_number':entry.roll_number,'entry_time':entry.entry_time} for entry in late_entries]
+    return jsonify({'late_entries_last_night':late_students})
+
+
+
